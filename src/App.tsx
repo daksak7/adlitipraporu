521b650d39c3d2d4ae4803d52305c94f1550fe2b
daksak <dogancan_aksak@hotmail.com>
2023-01-11 05:48:20 +0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import {AddReport} from "./components/AddReport";
-import 'rsuite/dist/rsuite.min.css'
+import "rsuite/dist/rsuite.min.css";
+import { AddReport } from "./components/AddReport";
+import Nav from "./components/Nav";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <>
-      <AddReport />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav/>}>
+            <Route index element={<Dashboard />} />
+            <Route path="/addreport" element={<AddReport />} />
+            <Route path="*" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
--- src/components/AddReport.tsx
+++ src/components/AddReport.tsx
@@ -10,31 +10,56 @@ import {
   RadioGroup,
   TagPicker,
 } from "@rsuite/formik";
-import { AutoComplete, Button, Container, Divider, Radio, Cascader } from "rsuite";
+import {
+  AutoComplete,
+  Button,
+  Container,
+  Divider,
+  Radio,
+  Cascader,
+} from "rsuite";
 import { Cinsiyet, KanGrubu, KanGrubuRh } from "../ReportDetails";
-
 
 interface MyFormValues {
   adi: string;
   soyadi: string;
   yas: number | null;
   cinsiyet: string;
-  kanGrubu: string;
-  kanGrubuRh: string;
+  kanGrubu: string | null;
+  kanGrubuRh: string | null;
   darpDurumu: boolean | null;
   gelisNedeni: string;
   gelisNedeniAciklama: string;
-  sikayet:string;
+  sikayet: string;
   uygunOrtamSaglandi: boolean | null;
-  odadaBulunanlar: object|null;
-  doktorAdi:string;
-  organizasyon:string;
+  odadaBulunanlar: object | null;
+  doktorAdi: string;
+  organizasyon: string;
 }
+
+export const previewValues = {
+  adi: "",
+  soyadi: "",
+  yas: null,
+  cinsiyet: "",
+  kanGrubu: "",
+  kanGrubuRh: "",
+  darpDurumu: null,
+  gelisNedeni: "",
+  gelisNedeniAciklama: "",
+  sikayet: "",
+  uygunOrtamSaglandi: null,
+  odadaBulunanlar: null,
+  doktorAdi: "",
+  organizasyon: "",
+};
 
 export const AddReport: React.FC<{}> = () => {
   const [darpDurumu, setDarpDurumu] = useState();
   const [uygunOrtam, setUygunOrtam] = useState(false);
   const [next, setNext] = useState(false);
+  const [prevalues, setPrevalues] = useState(previewValues);
+  const [pre, setPre] = useState(false);
 
   const initialValues: MyFormValues = {
     adi: "",
@@ -46,176 +71,217 @@ export const AddReport: React.FC<{}> = () => {
     darpDurumu: null,
     gelisNedeni: "",
     gelisNedeniAciklama: "",
-    sikayet:"",
+    sikayet: "",
     uygunOrtamSaglandi: null,
     odadaBulunanlar: null,
-    doktorAdi:"",
-    organizasyon:""
+    doktorAdi: "",
+    organizasyon: "",
   };
   return (
     <div>
       <h2>Yeni rapor kaydı</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={SampleSchema}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
-      >
-        {({ errors, touched, isValidating }) => (
-        <Container style={{ padding: 30 }}>
-        
-          <Form>
-            <h4>Hasta Bilgileri</h4>
-            <label htmlFor="adi">Adı</label>
-            <Field name="adi" id="adi" component={Input} />
-            {errors.adi && touched.adi ? <div>{errors.adi}</div> : null}
-            <label htmlFor="soyadi">Soyadı</label>
-            <Field name="soyadi" id="soyadi" component={Input} />
-            {errors.soyadi && touched.soyadi ? <div>{errors.soyadi}</div> : null}
-            <label htmlFor="yas">Yaşı</label>
-            <Field name="yas" id="yas" component={InputNumber} />
-            {errors.yas && touched.yas ? <div>{errors.yas}</div> : null}
-            <label htmlFor="cinsiyet"> Cinsiyet </label>
-            <Field
-              name="cinsiyet"
-              id="cinsiyet"
-              data={genders}
-              component={SelectPicker}
-            />
-            {errors.cinsiyet && touched.cinsiyet ? <div>{errors.cinsiyet}</div> : null}
-            <br />
-            <label htmlFor="kanGrubu"> Kan Grubu </label>
-             <Field
-              searchable={false}
-              data={kan} 
-              name="kanGrubu"
-              id="kanGrubu"
-              component={SelectPicker}
-            />
-            {errors.kanGrubu && touched.kanGrubu ? <div>{errors.kanGrubu}</div> : null}
-            <Divider />
-            <h4>Rapor Bilgileri</h4>
-            <Field
-              inline
-              id="darpDurumu"
-              name="darpDurumu"
-              value={darpDurumu}
-              component={RadioGroup}
-              onChange={setDarpDurumu}
-            >
-              <Field value={true} component={Radio}>
-                Evet
-              </Field>
-              <Field value={false} component={Radio}>
-                Hayır
-              </Field>
-            </Field>
-            {errors.darpDurumu && touched.darpDurumu ? <div>{errors.darpDurumu}</div> : null}
-            
-            {!next ? (
-              <Button
-                onClick={() => {
-                  setNext(true);
-                }}
-                disabled={darpDurumu === undefined}
-              >
-                Devam
-              </Button>
-            ) : null}
-            {next ? 
-                <>
-                <label htmlFor="gelisNedeni"> Geliş nedeni </label>
-                <br />
+      {pre === false ? (
+        <Formik
+          initialValues={prevalues || initialValues}
+          enableReinitialize
+          validationSchema={SampleSchema}
+          onSubmit={(values, actions) => {
+            // console.log({ values, actions });
+            setPrevalues(values);
+            setPre(true);
+            //  alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }}
+        >
+          {({ errors, touched, isValidating }) => (
+            <Container style={{ padding: 30 }}>
+              <Form>
+                <h4>Hasta Bilgileri</h4>
+                <label htmlFor="adi">Adı</label>
+                <Field name="adi" id="adi" component={Input} />
+                {errors.adi && touched.adi ? <div>{errors.adi}</div> : null}
+                <label htmlFor="soyadi">Soyadı</label>
+                <Field name="soyadi" id="soyadi" component={Input} />
+                {errors.soyadi && touched.soyadi ? (
+                  <div>{errors.soyadi}</div>
+                ) : null}
+                <label htmlFor="yas">Yaşı</label>
+                <Field name="yas" id="yas" component={InputNumber} />
+                {errors.yas && touched.yas ? <div>{errors.yas}</div> : null}
+                <label htmlFor="cinsiyet"> Cinsiyet </label>
                 <Field
-                  id="gelisNedeni"
-                  name="gelisNedeni"
-                  data={gelis}
+                  name="cinsiyet"
+                  id="cinsiyet"
+                  data={genders}
                   component={SelectPicker}
                 />
-                {errors.gelisNedeni && touched.gelisNedeni ? <div>{errors.gelisNedeni}</div> : null}
+                {errors.cinsiyet && touched.cinsiyet ? (
+                  <div>{errors.cinsiyet}</div>
+                ) : null}
                 <br />
-                <label htmlFor="gelisNedeniAciklama">Geliş nedeni açıklama</label>
+                <label htmlFor="kanGrubu"> Kan Grubu </label>
                 <Field
-                  placeholder="Opsiyonel"
-                  id="gelisNedeniAciklama"
-                  name="gelisNedeniAciklama"
-                  component={Input}
+                  searchable={false}
+                  data={kan}
+                  name="kanGrubu"
+                  id="kanGrubu"
+                  component={SelectPicker}
                 />
-                <label htmlFor="sikayet">Şikayet</label>
+                {errors.kanGrubu && touched.kanGrubu ? (
+                  <div>{errors.kanGrubu}</div>
+                ) : null}
+                <Divider />
+                <h4>Rapor Bilgileri</h4>
                 <Field
-                  id="sikayet"
-                  name="sikayet"
-                  component={Input}
-                />
-                {errors.sikayet && touched.sikayet ? <div>{errors.sikayet}</div> : null}
-                <label htmlFor="uygunOrtamSaglandi"> Uygun ortam sağlandı mı? </label>
-                <br />
-                <Field
-                  value={uygunOrtam}
-                  onChange={setUygunOrtam}
-                  id="uygunOrtamSaglandi"
-                  name="uygunOrtamSaglandi"
-                  size="lg"
-                  checkedChildren="Evet"
-                  unCheckedChildren="Hayır"
-                  component={Toggle}
-                />
-                {errors.uygunOrtamSaglandi && touched.uygunOrtamSaglandi ? <div>{errors.uygunOrtamSaglandi}</div> : null}
-                <br />
-                <label htmlFor="odadaBulunanlar"> Odada bulunanlar </label>
-                <br />
-                <Field
-                  id="odadaBulunanlar"
-                  name="odadaBulunanlar"
-                  data={bulunanlar}
-                  component={TagPicker}
-                />
-                {errors.odadaBulunanlar && touched.odadaBulunanlar ? <div>Odada bulunanlar kısmı boş bırakılamaz</div> : null}
-                <br />
-                <label htmlFor="doktorAdi">Doktor adı</label>
-                <Field id="doktorAdi" name="doktorAdi" component={Input} />
-                {errors.doktorAdi && touched.doktorAdi ? <div>{errors.doktorAdi}</div> : null}
-                <label htmlFor="organizasyon">Organizasyon</label>
-                <Field id="organizasyon" name="organizasyon" component={Input} />
-                {errors.organizasyon && touched.organizasyon ? <div>{errors.organizasyon}</div> : null}
-                <Button type="submit">Raporu kaydet</Button>
-              </>
-            : null}
-          </Form>
-          
+                  inline
+                  id="darpDurumu"
+                  name="darpDurumu"
+                  value={darpDurumu}
+                  component={RadioGroup}
+                  onChange={setDarpDurumu}
+                >
+                  <Field value={true} component={Radio}>
+                    Evet
+                  </Field>
+                  <Field value={false} component={Radio}>
+                    Hayır
+                  </Field>
+                </Field>
+                {errors.darpDurumu && touched.darpDurumu ? (
+                  <div>{errors.darpDurumu}</div>
+                ) : null}
+
+                {!next ? (
+                  <Button
+                    onClick={() => {
+                      setNext(true);
+                    }}
+                    disabled={darpDurumu === undefined}
+                  >
+                    Devam
+                  </Button>
+                ) : null}
+                {next ? (
+                  <>
+                    <label htmlFor="gelisNedeni"> Geliş nedeni </label>
+                    <br />
+                    <Field
+                      id="gelisNedeni"
+                      name="gelisNedeni"
+                      data={gelis}
+                      component={SelectPicker}
+                    />
+                    {errors.gelisNedeni && touched.gelisNedeni ? (
+                      <div>{errors.gelisNedeni}</div>
+                    ) : null}
+                    <br />
+                    <label htmlFor="gelisNedeniAciklama">
+                      Geliş nedeni açıklama
+                    </label>
+                    <Field
+                      placeholder="Opsiyonel"
+                      id="gelisNedeniAciklama"
+                      name="gelisNedeniAciklama"
+                      component={Input}
+                    />
+                    <label htmlFor="sikayet">Şikayet</label>
+                    <Field id="sikayet" name="sikayet" component={Input} />
+                    {errors.sikayet && touched.sikayet ? (
+                      <div>{errors.sikayet}</div>
+                    ) : null}
+                    <label htmlFor="uygunOrtamSaglandi">
+                      {" "}
+                      Uygun ortam sağlandı mı?{" "}
+                    </label>
+                    <br />
+                    <Field
+                      value={uygunOrtam}
+                      onChange={setUygunOrtam}
+                      id="uygunOrtamSaglandi"
+                      name="uygunOrtamSaglandi"
+                      size="lg"
+                      checkedChildren="Evet"
+                      unCheckedChildren="Hayır"
+                      component={Toggle}
+                    />
+                    {errors.uygunOrtamSaglandi && touched.uygunOrtamSaglandi ? (
+                      <div>{errors.uygunOrtamSaglandi}</div>
+                    ) : null}
+                    <br />
+                    <label htmlFor="odadaBulunanlar"> Odada bulunanlar </label>
+                    <br />
+                    <Field
+                      id="odadaBulunanlar"
+                      name="odadaBulunanlar"
+                      data={bulunanlar}
+                      component={TagPicker}
+                    />
+                    {errors.odadaBulunanlar && touched.odadaBulunanlar ? (
+                      <div>Odada bulunanlar kısmı boş bırakılamaz</div>
+                    ) : null}
+                    <br />
+                    <label htmlFor="doktorAdi">Doktor adı</label>
+                    <Field id="doktorAdi" name="doktorAdi" component={Input} />
+                    {errors.doktorAdi && touched.doktorAdi ? (
+                      <div>{errors.doktorAdi}</div>
+                    ) : null}
+                    <label htmlFor="organizasyon">Organizasyon</label>
+                    <Field
+                      id="organizasyon"
+                      name="organizasyon"
+                      component={Input}
+                    />
+                    {errors.organizasyon && touched.organizasyon ? (
+                      <div>{errors.organizasyon}</div>
+                    ) : null}
+                    <Button type="submit">Önizleme</Button>
+                  </>
+                ) : null}
+              </Form>
+            </Container>
+          )}
+        </Formik>
+      ) : (
+        <Container>
+          <>
+            <h4>Rapor Önizleme</h4>
+
+            {JSON.stringify(prevalues, null, 2)}
+            <br />
+            <Button
+              appearance="subtle"
+              onClick={() => {
+                setPre(false);
+              }}
+            >
+              Düzenlemeye geri dön
+            </Button>
+            <Button appearance="primary">Formu kaydet</Button>
+          </>
         </Container>
-        )}
-      </Formik>
+      )}
     </div>
   );
 };
 
-
-const genders = [Cinsiyet.Erkek,Cinsiyet.Kadin].map((item) => ({
+const genders = [Cinsiyet.Erkek, Cinsiyet.Kadin].map((item) => ({
   label: item,
   value: item,
 }));
 
-
-
-const kan = 
-[
-  KanGrubu.A+KanGrubuRh.RhPozitif,
-  KanGrubu.A+KanGrubuRh.RhNegatif,
-  KanGrubu.B+KanGrubuRh.RhPozitif,
-  KanGrubu.B+KanGrubuRh.RhNegatif,
-  KanGrubu.AB+KanGrubuRh.RhPozitif,
-  KanGrubu.AB+KanGrubuRh.RhNegatif,
-  KanGrubu.O+KanGrubuRh.RhPozitif,
-  KanGrubu.O+KanGrubuRh.RhNegatif,
-].map((item)=>({
-  label:item,
-  value:item
-}))
-
+const kan = [
+  KanGrubu.A + KanGrubuRh.RhPozitif,
+  KanGrubu.A + KanGrubuRh.RhNegatif,
+  KanGrubu.B + KanGrubuRh.RhPozitif,
+  KanGrubu.B + KanGrubuRh.RhNegatif,
+  KanGrubu.AB + KanGrubuRh.RhPozitif,
+  KanGrubu.AB + KanGrubuRh.RhNegatif,
+  KanGrubu.O + KanGrubuRh.RhPozitif,
+  KanGrubu.O + KanGrubuRh.RhNegatif,
+].map((item) => ({
+  label: item,
+  value: item,
+}));
 
 const gelis = [
   "Etkili Eylem",
@@ -240,4 +306,3 @@ const bulunanlar = [
   label: item,
   value: item,
 }));
-
